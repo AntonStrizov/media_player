a10149e81754859292a9d4d59603d27b50dcb875
AntonStrizov <as386176@gmail.com>
2023-12-31 15:03:47 +0200

    done

--- player.js
+++ player.js
@@ -4,42 +4,35 @@ const media_player_line = document.querySelector('#media_player_line');
 const playStop_button_img =  document.querySelector('.playStop_button_img');
 let btn_pressing = 0;
 let f;
-play_stop_btn.addEventListener('click', function(){
-if(btn_pressing == 0){
-    btn_pressing++;
-    playStop_button_img.src = imgs_links[btn_pressing];
-    // media_player_on('on');
-    f = setInterval(function(){
+let isPaused = true;
+f = setInterval(function(){
+    if(isPaused == false){
         let player_line_point = parseInt(media_player_line.value) + 1;
-        media_player_line.value = player_line_point
-        console.log(media_player_line.value);
-     },1000)
-     media_player_line.addEventListener('mousedown', function(){
-        clearInterval(f)
-        //media_player_line.removeEventListener('mouseup');
-     })
-     media_player_line.addEventListener('mouseup', function(){
-        f = setInterval(function(){
-            let player_line_point = parseInt(media_player_line.value) + 1;
-            media_player_line.value = player_line_point
-            console.log(media_player_line.value);
-         },1000)
-         //media_player_line.removeEventListener('mousedown', addEventListener, true);
-     })
-}else{
-    btn_pressing--;
-    playStop_button_img.src = imgs_links[btn_pressing];
-    // media_player_on('off')
-    clearInterval(f)
-}
+        media_player_line.value = player_line_point ;
+        if(media_player_line.value == 100){
+            isPaused = true;
+            btn_pressing--;
+        playStop_button_img.src = imgs_links[btn_pressing];
+        }
+    }
+    },1000)
+play_stop_btn.addEventListener('click', function(){
+    if(btn_pressing == 0){
+        btn_pressing++;
+        playStop_button_img.src = imgs_links[btn_pressing];
+        isPaused = false;
+    }else{
+        btn_pressing--;
+        playStop_button_img.src = imgs_links[btn_pressing];
+        isPaused = true;
+    }
 })
-
-// function media_player_on(ststus){
-//     if(ststus == 'on'){ 
-//     let f = setInterval(function(){
-//         console.log(media_player_line.value);
-//     },1000)
-//     }else{
-//         clearInterval
-//     }
-// }
+media_player_line.addEventListener('mousedown', function(){
+    isPaused = true;
+})
+media_player_line.addEventListener('mouseup', function(){
+    if(btn_pressing == 1){
+        isPaused = false;
+    }
+    
+})
